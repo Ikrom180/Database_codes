d25ca75cc9e5e7cccf74f87497588223778ae73c
ikrom724 <ikrompersonal@gmail.com>
2026-06-19 17:22:55 +0500

    works with Indexation

--- indexation.sql
+++ indexation.sql
@@ -9,7 +9,18 @@
 -- show indexes from customers => shows index has or not in table
 
 # Creating indexing
+
+use mydb;
+
 Create index last_name_inx
 on customers(last_name);
 
-show indexes from customers
+show indexes from customers;
+
+alter table customers
+DROP index last_name_inx;
+
+create index last_name_first_name_idx
+on customers(last_name, first_name);
+
+show indexes from customers;
---
+++ python_codes/Python_Lambda.py
@@ -0,0 +1,9 @@
+# Lambda
+
+# def add(x, y):
+#     return x + y
+# print(add(1, 2))
+
+
+
+print((lambda x,y: x+y)(1,2))
